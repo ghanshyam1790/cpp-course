7f39b48319b3a25da2fdb69f6ad08959ae21066f
Ghanshyam Pandey <ghanshyampandey8697@gmail.com>
2026-07-22 09:23:14 +0530

    Leant about classes in OOP

--- exercise3.cpp
+++ exercise3.cpp
@@ -1,12 +1,38 @@
-#include <iostream>
+//MAKING A CLASS OF DOG;  GOT HW FROM tut21
+#include<iostream>
 using namespace std;
+
+class Animal{
+    private:
+        int legs, eyes, ears;
+
+    public:
+        int sound, speed;
+    void setData(int a1, int b1, int c1);
+    void getData()
+     {
+        cout<<"The total legs are "<<legs<<endl;
+        cout<<"The total eyes are "<<eyes<<endl;
+        cout<<"The total ears are "<<ears<<endl;
+        cout<<"The sound is "<<sound<<"Hz"<<endl;
+        cout<<"The average speed is "<<speed<<"kmph"<<endl;
+    }
+};
+
+void Animal :: setData(int a1, int b1, int c1){
+    legs=a1;
+    eyes=b1;
+    ears=c1;
+}
+
 
 int main()
 {
-  for (int i = 1; i <=10; i++)
-  {
-    cout<<i*6<<endl;
-  }
-  
+    Animal Dog;
+    Dog.sound= 56;
+    Dog.speed= 25;
+    Dog.setData(4, 2, 2);
+
+    Dog.getData();
     return 0;
 }
